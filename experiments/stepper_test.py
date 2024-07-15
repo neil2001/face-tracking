06c20f9ade3ff1d8ae41b9f1d22cdd4e07fc9ca1
Neil Xu <xu3033@gmail.com>
2024-07-15 15:46:30 -0500

    path fix

--- experiments/stepper_test.py
+++ experiments/stepper_test.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 from FaceTracker.src.motor.byj_motor_control import BYJMotor
 from FaceTracker.src.motor.motorcontroller import MotorController
 
