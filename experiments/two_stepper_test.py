8dd1df6e981f9200d445f979a761279e26f531a4
Neil Xu <xu3033@gmail.com>
2024-07-16 18:26:43 +0100

    troubleshooting two motors

--- experiments/two_stepper_test.py
+++ experiments/two_stepper_test.py
@@ -3,26 +3,28 @@ import os
 import concurrent.futures
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 from src.motor.motorcontroller import MotorController
 
 pins = [17,18,27,22]
 motor = MotorController(pins=pins, gear_ratio=3, min_angle=-90, max_angle=90)
 
-pins2 = [17,18,27,22]
+pins2 = [6,13,19,26]
 motor2 = MotorController(pins=pins2, gear_ratio=3, min_angle=-90, max_angle=90)
 
-motor.rotate(90)
-motor.rotate(-180)
-motor.rotate(90)
+motor.rotate(45)
+motor.rotate(-90)
+motor.rotate(45)
 
-motor2.rotate(90)
-motor2.rotate(-180)
-motor2.rotate(90)
+motor2.rotate(45)
+motor2.rotate(-90)
+motor2.rotate(45)
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     executor.submit(motor.rotate, 90)
     executor.submit(motor2.rotate, 90)
-    executor.submit(motor.rotate, -90)
-    executor.submit(motor2.rotate, -90)
 
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.submit(motor.reset)
+    executor.submit(motor2.reset)
