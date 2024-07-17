b6c4fb084bee68ba01c23f6bf98835bf58fa4b1b
Neil Xu <xu3033@gmail.com>
2024-07-17 17:08:40 +0100

    multithreading motors

--- experiments/stepper_test.py
+++ experiments/stepper_test.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 from src.motor.byj_motor_control import BYJMotor
 from src.motor.motorcontroller import MotorController
@@ -14,9 +15,10 @@ pins = [17,18,27,22]
 # motor.motor_run(pins, wait=0.001, steps=steps)
 
 motor = MotorController(pins=pins, gear_ratio=3, min_angle=-90, max_angle=90)
-motor.rotate(90)
-motor.rotate(-180)
-motor.rotate(90)
-motor.rotate(45)
-motor.rotate(-90)
-motor.rotate(45)
+motor.rotate(90, verbose=True)
+motor.rotate(-180, verbose=True)
+motor.rotate(90, verbose=True)
+motor.rotate(45, verbose=True)
+motor.rotate(-90, verbose=True)
+motor.rotate(45, verbose=True)
+motor.reset()
--- src/motor/motorcontroller.py
+++ src/motor/motorcontroller.py
@@ -1,5 +1,7 @@
+import threading
 from motor.byj_motor_control import BYJMotor
 
+STEPS_PER_ROTATION = 512
 FULL_ROTATION_STEPS = 512*3
 FULL_ROTATION_DEGREES = 360
 
@@ -11,29 +13,57 @@ class MotorController(object):
         self.current_angle = 0
         self.min_angle = min_angle
         self.max_angle = max_angle
+        
+        self.motor_thread = None
+        self.lock = threading.Lock()
 
     def rotate(self, angle, verbose=False):
         self.motor.motor_stop() # DOES THIS WORK WITH MULTITHREADING?
+        
+        with self.lock:
+            if self.motor_thread is not None:
+                self.motor_thread.join()
+                steps_taken = self.motor.steps_taken
+                angle_traversed = steps_taken / STEPS_PER_ROTATION / self.gear_ratio * FULL_ROTATION_DEGREES
+                if verbose:
+                    print("angle_traversed", angle_traversed)
+            
+            if self.current_angle == self.max_angle and angle > 0:
+                print("max angle reached")
+                return
+            if self.current_angle == self.min_angle and angle < 0: 
+                print("min angle reached")
+                return
 
-        if self.current_angle == self.max_angle and angle > 0:
-            return
-        if self.current_angle == self.min_angle and angle < 0: 
-            return
+            next_angle = max(self.min_angle, min(self.max_angle, self.current_angle + angle))
 
-        next_angle = max(self.min_angle, min(self.max_angle, self.current_angle + angle))
-
-        if verbose: 
-            print("angle to move", angle)
-            print("current angle", self.current_angle)
-            print("next angle", next_angle)
-        steps = int(self.gear_ratio * ((next_angle - self.current_angle) / FULL_ROTATION_DEGREES) * 512)
-        if verbose: 
-            print("steps to move", steps)
-        self.motor.motor_run(self.pins, steps=abs(steps), ccwise=steps<0)
-        self.current_angle = next_angle
-        if verbose:
-            print("new_angle", self.current_angle)
+            if verbose: 
+                print("angle to move", angle)
+                print("current angle", self.current_angle)
+                print("next angle", next_angle)
+            steps = int(self.gear_ratio * ((next_angle - self.current_angle) / FULL_ROTATION_DEGREES) * STEPS_PER_ROTATION)
+            if verbose: 
+                print("steps to move", steps)
+                
+            self.motor_thread = threading.Thread(target=self.motor.motor_run, args=(self.pins,), kwargs={'steps': abs(steps), 'ccwise':steps<0})
+                
+            self.motor_thread.start()
+            
+            # ~ steps_taken = self.motor.motor_run(self.pins, steps=abs(steps), ccwise=steps<0)
+            
+            # ~ angle_traversed = steps_taken / STEPS_PER_ROTATION / self.gear_ratio * FULL_ROTATION_DEGREES
+            # ~ if verbose:
+                # ~ print("angle traversed", angle_traversed)
+                
+            self.current_angle = next_angle
+            
+            if verbose:
+                print("new_angle", self.current_angle)
+                print("\n")
 
     def reset(self):
-        self.rotate(-self.current_angle)
-        self.current_angle = 0
+        with self.lock:
+            angle = -self.current_angle
+        self.rotate(angle)
+        with self.lock:
+            assert(self.current_angle == 0)
