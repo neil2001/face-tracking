6371471e9f6ae900b7ddd599fadb692779704b67
Neil Xu <xu3033@gmail.com>
2024-07-17 10:06:49 -0500

    updated multi thread test

--- experiments/two_stepper_multi_thread.py
+++ experiments/two_stepper_multi_thread.py
@@ -2,6 +2,7 @@ import sys
 import os
 import concurrent.futures
 import threading
+import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
@@ -16,12 +17,21 @@ motor2 = MotorController(pins=pins2, gear_ratio=3, min_angle=-90, max_angle=90)
 
 motor_thread = threading.Thread(target=motor.rotate, args=(90,))
 motor2_thread = threading.Thread(target=motor2.rotate, args=(90,))
+motor2_thread2 = threading.Thread(target=motor2.rotate, args=(-135,))
 
 motor_thread.start()
 motor2_thread.start()
 
 motor_thread.join()
 motor2_thread.join()
+
+motor_thread.start()
+motor_thread.join()
+
+motor2_thread.start()
+motor2_thread2.start()
+motor2_thread.join()
+motor2_thread2.join()
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     executor.submit(motor.reset)
